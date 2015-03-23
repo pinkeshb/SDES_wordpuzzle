a4532c45c2b1bab7a866c2866ca5649265c85883
Pinkesh Barsopia <barsopia@gmail.com>
2015-03-24 01:40:41 +0530

    Adding a test case to test_get_word() and correcting few syntatical and logical mistakes

--- test_charmat.py
+++ test_charmat.py
@@ -17,22 +17,34 @@ class TestCharMatClass(unittest.TestCase):
 		self.n=15
 		self.chararray[0:3]=['T','E','A']
 		C =charmat.CharMat()
-		C.set_word("TEA",(0,0),(0,2))
+		C.set_word("TEA",(0,0),(2,0))
 		self.assertEqual(self.chararray,C.chararray)
+
 		self.chararray=[' ']*15*15
 		self.n=15
-		self.chararray[94]='T'
-		self.chararray[110]='E'
-		self.chararray[126]='A'
+		self.chararray[0]='T'
+		self.chararray[16]='E'
+		self.chararray[32]='A'
+		C =charmat.CharMat()
+		C.set_word("TEA",(0,0),(2,2))
+		self.assertEqual(self.chararray,C.chararray)
+
+		self.chararray=[' ']*15*15
+		self.n=15
+		self.chararray[95]='T'
+		self.chararray[111]='E'
+		self.chararray[127]='A'
 		C =charmat.CharMat()
 		C.set_word("TEA",(5,6),(7,8))
 		self.assertEqual(self.chararray,C.chararray)
 
-		self.assertRaises(ValueError,C.set_word,("TEA",(5,6),(8,8))) #invalid position
+		self.assertRaises(ValueError,C.set_word,"TEA",(5,6),(8,8)) #invalid position
 
-		self.assertRaises(ValueError,C.set_word,("TEAA",(5,6),(7,8))) #length doesn't match
+		self.assertRaises(ValueError,C.set_word,"TEA",(15,8),(15,10)) #invalid position
 
-		self.assertRaises(ValueError,C.set_word,("TEA",(0,0),(0,13))) #length doesn't match
+		self.assertRaises(ValueError,C.set_word,"TEAA",(5,6),(7,8)) #length doesn't match
+
+		self.assertRaises(ValueError,C.set_word,"TEA",(0,0),(0,13)) #length doesn't match
 
 	def test_get_word(self):
 		C =charmat.CharMat()
@@ -42,8 +54,8 @@ class TestCharMatClass(unittest.TestCase):
 		self.assertEqual( "TEA",C.get_word((5,6),(7,6)) )
 		C.set_word("TEA",(5,6),(5,8))
 		self.assertEqual( "TEA",C.get_word((5,6),(5,8)) )
-		self.assertRaises(ValueError,C.get_word,((5,6),(8,8)) )
-		# self.assertRaises(ValueError,C.get_word,((5,6),(8,8)) )
+		self.assertRaises(ValueError,C.get_word,(5,6),(8,8)) #invalid position
+		self.assertRaises(ValueError,C.get_word,(15,6),(8,8) ) #invalid position
 
 	def test_fill_characters_randomly(self):
 		C=charmat.CharMat()
