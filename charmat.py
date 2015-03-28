331f04c7763e7911a460faf13cd0fc2071bbb5bb
Pinkesh Barsopia <barsopia@gmail.com>
2015-03-28 11:57:44 +0530

    Editing charmat.py and test_charmat.py for pep8 convention

--- charmat.py
+++ charmat.py
@@ -1,104 +1,127 @@
 from random import randint
-DEFAULT_n=15
+
+
+DEFAULT_SIZE = 15
+
+
 class CharMat(object):
-	def __init__(self,n=DEFAULT_n):
-		"""adds size of puzzle n and character array to object,
-		fills matrix with spaces"""
-		self.chararray=[' ']*n*n
-		self.n=n
-	def get_word(self,(x_start,y_start),(x_end,y_end)):
-		"""returns word from matrix form start to end coordinates 
-		after checking validity of coordinates"""
-		 # validity checking
-		if not( 0 <= x_start <=self.n and 0 <= x_end <=self.n and 0 <= y_start <=self.n and 0 <= y_end <=self.n):
-			raise	ValueError
-
-		 # horz and vert direction words 
-		word=[]
-		if x_start==x_end:
-			if y_start<y_end:
-				for i in range(y_start,y_end+1):
-					word.append(self.chararray[x_start+i*self.n])
-				return "".join(word)
-
-			else:
-				for i in range(y_start,y_end+1):
-					word.append(self.chararray[x_start+i*self.n])
-					word.reverse()
-				return "".join(word)
-		if y_start==y_end:
-			if x_start<x_end:
-				for i in range(x_start,x_end+1):
-					word.append(self.chararray[y_start*self.n+i])
-				return "".join(word)
-			else:
-				for i in range(x_start,x_end+1):
-					word.append(self.chararray[y_start*self.n+i])
-					word.reverse()
-				return "".join(word)
-
-		 # validity chararray for diagonal words 
-		if not(abs(x_start-x_end) == abs(y_start-y_end)):
-			raise ValueError
-
-		 # diagonal words
-		for i in range(abs(x_start-x_end)+1):
-			word.append(self.chararray[ 
-				( x_start+i*(x_end- x_start)/abs(x_end- x_start) ) 
-				+ (y_start+i*(y_end- y_start)/abs(y_end- y_start)) *self.n  
-			])
-
-		return "".join(word)
-
-	def set_word(self,word,(x_start,y_start),(x_end,y_end)):
-		"""set word in matrix form start to end coordinates 
-		after checking validity of coordinates"""
-		if not( 0 <= x_start <self.n and 0 <= x_end <self.n and 0 <= y_start <self.n and 0 <= y_end <self.n):
-			raise	ValueError
-		if x_start == x_end:
-			if abs(y_start-y_end) == (len(word)-1):
-				for i in range(len(word)):
-					self.chararray[x_start + (y_start+i*(y_end- y_start)/abs(y_end- y_start)) *self.n  ] = word[i]
-				return
-			else:
-				raise ValueError
-		if y_start == y_end:
-			if abs(x_start-x_end) == (len(word)-1):
-				for i in range(len(word)):
-					self.chararray[y_start*self.n + (x_start+i*(x_end- x_start)/abs(x_end- x_start)) ] = word[i]
-				return 
-			else:
-				raise ValueError
-		if not( abs(x_start-x_end) == abs(y_start-y_end)== (len(word)-1)):
-			raise ValueError
-
-		for i in range(len(word)):
-			self.chararray[ 
-				( x_start+i*(x_end- x_start)/abs(x_end- x_start) ) 
-				+ (y_start+i*(y_end- y_start)/abs(y_end- y_start)) *self.n  
-			] = word[i]
 
 
-	def fill_characters_randomly(self,string):
-		"""fills all the spaces in matrix with randomly selected 
-		characters form string"""
-		for i in range(self.n*self.n):
-			if self.chararray[i]==' ':
-				self.chararray[i]= string[randint(0,len(string)-1)]
-	def term_display(self):
-		"""display matrix on terminal"""
-		print self.n,"*",self.n, "puzzle"
-		for i in range(self.n):
-			for j in range(self.n):
-				print self.chararray[j+i*self.n],
-			print " "
+    def __init__(self, n=DEFAULT_SIZE):
+        """adds size of puzzle n and character array to object,
+        fills matrix with spaces
+        """
+        self.char_array = [' '] * n * n
+        self.n = n
 
+    def get_word(self, (x_start, y_start), (x_end, y_end)):
+        """returns word from matrix form start to end coordinates 
+        after checking validity of coordinates
+        """
+        word = []
+
+          # validity checking
+        if not((0 <= x_start <= self.n and 0 <= x_end <= self.n) and
+               (0 <= y_start <= self.n and 0 <= y_end <= self.n)):
+            raise ValueError
+
+          # horz and vert direction words
+        if x_start == x_end:
+            if y_start < y_end:
+                for i in range(y_start, y_end + 1):
+                    word.append(self.char_array[x_start + i * self.n])
+                return "".join(word)
+
+            else:
+                for i in range(y_start, y_end + 1):
+                    word.append(self.char_array[x_start + i * self.n])
+                    word.reverse()
+                return "".join(word)
+        if y_start == y_end:
+            if x_start < x_end:
+                for i in range(x_start, x_end + 1):
+                    word.append(self.char_array[y_start * self.n + i])
+                return "".join(word)
+            else:
+                for i in range(x_start, x_end + 1):
+                    word.append(self.char_array[y_start * self.n + i])
+                    word.reverse()
+                return "".join(word)
+
+         # diagonal words
+        if not(abs(x_start - x_end) == abs(y_start - y_end)):
+            raise ValueError
+        for i in range(abs(x_start - x_end) + 1):
+            word.append(self.char_array[
+                (x_start + i*(x_end-x_start) / abs(x_end-x_start))   + 
+                (y_start + i*(y_end-y_start) / abs(y_end-y_start)) * self.n
+                ])
+
+        return "".join(word)
+
+    def set_word(self, word, (x_start, y_start), (x_end, y_end)):
+        """set word in matrix form start to end coordinates 
+        after checking validity of coordinates
+        """
+          # validity check
+        if not((0 <= x_start < self.n and 0 <= x_end < self.n) and 
+               (0 <= y_start < self.n and 0 <= y_end < self.n)):
+            raise ValueError
+
+          # horz and vert direction words
+        if x_start == x_end:
+            if abs(y_start - y_end) == (len(word) - 1):
+                for i in range(len(word)):
+                    self.char_array[x_start + 
+                    (y_start + i*(y_end-y_start) /
+                     abs(y_end-y_start))*self.n] = word[i]  
+                return
+            else:
+                raise ValueError
+        if y_start == y_end:
+            if abs(x_start - x_end) == (len(word) - 1):
+                for i in range(len(word)):
+                    self.char_array[y_start*self.n +
+                        (x_start + i*(x_end-x_start) /
+                        abs(x_end-x_start))] = word[i]
+                return
+            else:
+                raise ValueError
+
+          # diagonal words
+        if not(abs(x_start-x_end) == abs(y_start-y_end) == (len(word)-1)):
+            raise ValueError
+        for i in range(len(word)):
+            self.char_array[
+                (x_start + i*(x_end-x_start) / abs(x_end-x_start))   +
+                (y_start + i*(y_end-y_start) / abs(y_end-y_start)) 
+                * self.n] = word[i]
+
+    def fill_characters_randomly(self, string):
+        """fills all the spaces in matrix with randomly selected 
+        characters form string
+        """
+        for i in range(self.n * self.n):
+            if self.char_array[i] == ' ':
+                self.char_array[i] = string[randint(0, len(string)-1)]
+
+    def term_display(self):
+        """display matrix on terminal"""
+        print self.n, "*", self.n, "puzzle"
+        for i in range(self.n):
+            for j in range(self.n):
+                print self.char_array[j + i*self.n],
+            print " "
+
+    def get_char(self, (x, y)):
+        """return character corresponding to x and y"""
+        return self.char_array[x + y*self.n]
 
 # HOW TO USE IT
-
 
 
 # C=CharMat(5)
 # C.set_word("teap",(0,0),(3,3))
 # C.fill_characters_randomly("gz")
 # C.term_display()
+# print C.get_char((0,0))
--- test_charmat.py
+++ test_charmat.py
@@ -1,67 +1,91 @@
 import charmat
 import unittest
+
+
 class TestCharMatClass(unittest.TestCase):
-	def setUp(self):
-		pass
-	def test_init(self):
-		self.chararray=[' ']*15*15
-		self.n=15
-		C=charmat.CharMat()
-		self.assertEqual((self.chararray,15),(C.chararray,C.n))
-		self.chararray=[' ']*25*25
-		self.n=25
-		C =charmat.CharMat(25)
-		self.assertEqual((self.chararray,25),(C.chararray,C.n))
-	def test_set_word(self):
-		self.chararray=[' ']*15*15
-		self.n=15
-		self.chararray[0:3]=['T','E','A']
-		C =charmat.CharMat()
-		C.set_word("TEA",(0,0),(2,0))
-		self.assertEqual(self.chararray,C.chararray)
-
-		self.chararray=[' ']*15*15
-		self.n=15
-		self.chararray[0]='T'
-		self.chararray[16]='E'
-		self.chararray[32]='A'
-		C =charmat.CharMat()
-		C.set_word("TEA",(0,0),(2,2))
-		self.assertEqual(self.chararray,C.chararray)
-
-		self.chararray=[' ']*15*15
-		self.n=15
-		self.chararray[95]='T'
-		self.chararray[111]='E'
-		self.chararray[127]='A'
-		C =charmat.CharMat()
-		C.set_word("TEA",(5,6),(7,8))
-		self.assertEqual(self.chararray,C.chararray)
-
-		self.assertRaises(ValueError,C.set_word,"TEA",(5,6),(8,8)) #invalid position
-
-		self.assertRaises(ValueError,C.set_word,"TEA",(15,8),(15,10)) #invalid position
-
-		self.assertRaises(ValueError,C.set_word,"TEAA",(5,6),(7,8)) #length doesn't match
-
-		self.assertRaises(ValueError,C.set_word,"TEA",(0,0),(0,13)) #length doesn't match
-
-	def test_get_word(self):
-		C =charmat.CharMat()
-		C.set_word("TEA",(5,6),(7,8))
-		self.assertEqual( "TEA",C.get_word((5,6),(7,8)) )
-		C.set_word("TEA",(5,6),(7,6))
-		self.assertEqual( "TEA",C.get_word((5,6),(7,6)) )
-		C.set_word("TEA",(5,6),(5,8))
-		self.assertEqual( "TEA",C.get_word((5,6),(5,8)) )
-		self.assertRaises(ValueError,C.get_word,(5,6),(8,8)) #invalid position
-		self.assertRaises(ValueError,C.get_word,(15,6),(8,8) ) #invalid position
-
-	def test_fill_characters_randomly(self):
-		C=charmat.CharMat()
-		C.fill_characters_randomly("asjf")
-		self.assertTrue(not(" " in C.chararray))
 
 
-if __name__ == '__main__':
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        self.char_array = [' ']*15*15
+        self.n = 15
+        char_mat = charmat.CharMat()
+        self.assertEqual((self.char_array, 15),
+                         (char_mat.char_array, char_mat.n))
+        self.char_array = [' ']*25*25
+        self.n = 25
+        char_mat = charmat.CharMat(25)
+        self.assertEqual((self.char_array, 25),
+                         (char_mat.char_array, char_mat.n))
+
+    def test_set_word(self):
+        self.char_array = [' ']*15*15
+        self.n = 15
+        self.char_array[0:3] = ['T', 'E', 'A']
+        char_mat =  charmat.CharMat()
+        char_mat.set_word("TEA", (0, 0), (2, 0))
+        self.assertEqual(self.char_array, char_mat.char_array)
+
+        self.char_array = [' ']*15*15
+        self.n = 15
+        self.char_array[0] = 'T'
+        self.char_array[16] = 'E'
+        self.char_array[32] = 'A'
+        char_mat =  charmat.CharMat()
+        char_mat.set_word("TEA", (0, 0), (2, 2))
+        self.assertEqual(self.char_array, char_mat.char_array)
+
+        self.char_array = [' ']*15*15
+        self.n = 15
+        self.char_array[95] = 'T'
+        self.char_array[111] = 'E'
+        self.char_array[127] = 'A'
+        char_mat =  charmat.CharMat()
+        char_mat.set_word("TEA", (5, 6), (7, 8))
+        self.assertEqual(self.char_array, char_mat.char_array)
+
+        # invalid position
+        self.assertRaises(ValueError, char_mat.set_word, 
+                          "TEA", (5, 6), (8, 8))
+
+        # invalid position
+        self.assertRaises(ValueError, char_mat.set_word, 
+                          "TEA", (15, 8), (15, 10))
+
+        # length doesn't match
+        self.assertRaises(ValueError, char_mat.set_word, 
+                          "TEAA", (5, 6), (7, 8))
+
+        # length doesn't match
+        self.assertRaises(ValueError, char_mat.set_word, 
+                          "TEA", (0, 0), (0, 13))
+
+    def test_get_word(self):
+        char_mat =  charmat.CharMat()
+        char_mat.set_word("TEA", (5, 6), (7, 8))
+        self.assertEqual("TEA", char_mat.get_word((5, 6), (7, 8)))
+        char_mat.set_word("TEA", (5, 6), (7, 6))
+        self.assertEqual("TEA", char_mat.get_word((5, 6), (7, 6)))
+        char_mat.set_word("TEA", (5, 6), (5, 8))
+        self.assertEqual("TEA", char_mat.get_word((5, 6), (5, 8)))
+        # invalid position
+        self.assertRaises(ValueError, char_mat.get_word, (5, 6), (8, 8))
+        # invalid position
+        self.assertRaises(ValueError, char_mat.get_word, (15, 6), (8, 8))
+
+    def test_fill_characters_randomly(self):
+        char_mat =  charmat.CharMat()
+        char_mat.fill_characters_randomly("asjf")
+        self.assertTrue(not(" " in char_mat.char_array))
+
+    def test_get_char(self):
+        char_mat =  charmat.CharMat()
+        char_mat.set_word("QWERTYUIOPASDFG", (0, 0), (14, 14))
+        self.assertEqual(char_mat.get_char((0, 0)), 'Q')
+        self.assertEqual(char_mat.get_char((14, 14)), 'G')
+        self.assertEqual(char_mat.get_char((4, 4)), 'T')
+        self.assertEqual(char_mat.get_char((10, 10)), 'A')
+if __name__  == '__main__':
     unittest.main()
