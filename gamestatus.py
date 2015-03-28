c35ee299c69babf340a9f1de7eceb7423847ef12
Pinkesh Barsopia <barsopia@gmail.com>
2015-03-28 12:53:28 +0530

    Adding new module "gamestatus" to hold status of game and structure of class Game_Status
    gamestatus contains score, word_list, char_mat, times_up,success and
    constructor and check_word methods

---
+++ gamestatus.py
@@ -0,0 +1,12 @@
+class GameStatus(object):
+
+
+    def __init__(self,word_list,char_mat):
+        """Initialize the score, word_list, char_mat, times_up,success"""
+
+    def check_word(self,p_1,p_2):
+        """Checks if word corresponding to p_1, p_2 is correct. 
+        And accordingly updates the word_list, score, success
+        if p_1==p_2==-1(indicating time's up) then updates the times_up
+        """ 
+
